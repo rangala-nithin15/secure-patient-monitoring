ac3886432bc08a7dc7c7df109f85a2af3b5d9d96
rangala nithin <158316590+rangala-nithin15@users.noreply.github.com>
2026-01-23 00:03:29 +0530

    Update auth.js

--- patient-monitoring-backend/routes/auth.js
+++ patient-monitoring-backend/routes/auth.js
@@ -1,1 +1,45 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+const SECRET = "SECRETKEY";
+
+/* REGISTER */
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    await user.save();
+    res.json({ message: "User created" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+/* LOGIN */
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.json({ message: "Invalid" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.json({ message: "Invalid" });
+
+  const token = jwt.sign({ id: user._id }, SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
 
