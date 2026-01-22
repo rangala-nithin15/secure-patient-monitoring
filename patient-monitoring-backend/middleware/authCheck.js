3d8d8fb4694dc71085c239aaf6ef49a6facdfa3a
rangala nithin <158316590+rangala-nithin15@users.noreply.github.com>
2026-01-22 23:56:08 +0530

    Create authCheck.js

---
+++ patient-monitoring-backend/middleware/authCheck.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const SECRET = "SECRETKEY";
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).json({ message: "No token" });
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ message: "Invalid token" });
+    req.user = decoded;
+    next();
+  });
+};
